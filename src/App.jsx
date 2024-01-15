cacca6ea5c4dfeb3c8154518d3bd82d63c562310
ElyanisJ <brancourt.julien@hotmail.fr>
2024-01-15 16:46:26 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Slider from "./components/Slider/Slider"
+
+function App() {
+
+  return (
+    <>
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <Slider />
+    </>
+  )
+}
+
+export default App
